0580a31e313b258fa2fb83e5259d2d7fc7b6efd7
Mohammed Hashim Nilger <hashimrangrezz786@gmail.com>
2026-08-27 16:05:24 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.set("trust proxy", 1);
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || " https://resume-craft-ai-v2.onrender.com",
+    origin: process.env.CLIENT_URL || "https://resume-craft-ai-v2.onrender.com",
     credentials: true,
   }),
 );
